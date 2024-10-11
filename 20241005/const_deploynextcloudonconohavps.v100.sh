b7e6854841c1a0fe858609c270b203b7fb2b21ef
matubara <matsubara.h@gmail.com>
2024-10-11 11:32:15 +0900

    新規作成

--- 20241005/const_deploynextcloudonconohavps.v100.sh
+++ 20241005/const_deploynextcloudonconohavps.v100.sh
@@ -1,34 +1,21 @@
 #Const Settings by user
 
 ###############################
-# Kusanagi構築時に必要な情報
+# NEXTCLOUD構築時に必要な情報
 ###############################
 #KUSANAGIパスワード
 kusanagipass="melb1999"
 #MYSQLルートパスワード
 dbrootpass="melb1999"
 #プロビジョニングフォルダ名
-kusanagiprofile="cloudtest"
+kusanagiprofile="nextcloud2024oct"
 #データベース名
-dbname="cloudtest"
+dbname="cloud2024oct"
 #データベースユーザ名
-dbuser="cloudtest"
+dbuser="cloud2024oct"
 #データベースパスワード名
 dbpass="melb1999"
 #FQDN（ドメイン）名
-FQDN="cloudtest.sincerew.online"
+FQDN="cloud2024oct.sincerew.online"
 #管理者メールアドレス
 email="admin@sincerew.online"
-
-###############################
-# Drupal構築時に必要な情報
-###############################
-#Drupalプロジェクトフォルダ名
-drupalproject=cloudtest
-#Drupalバージョン
-#drupalver=10.0.x-dev@dev
-drupalver=10.3.6
-#Drupalスーパーユーザ名
-drupalusr=sinceretechnology
-#Drupalスーパーユーザパスワード
-drupalpass="Melb#1999"
